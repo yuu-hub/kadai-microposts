abb08f284e7b33a50e99cae221f3a0276f122eac
y.sasamori <info.yuu09@gmail.com>
2021-12-26 02:56:04 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root to: 'toppages#index'
     
     get 'login', to: 'sessions#new'
@@ -8,7 +7,13 @@ Rails.application.routes.draw do
 
     
     get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :create]
+    resources :users, only: [:index, :show, :create] do
+      member do
+        get :followings
+        get :followers
+      end
+    end  
     
     resources :microposts, only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
 end
